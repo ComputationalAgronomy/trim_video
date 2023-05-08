4059c29ecfb07a7f283aa79007d5f4ac614aa07c
Shawn <shawn.for.office@gmail.com>
2023-05-08 22:37:34 +0800

    version_3 Added more conditional statements and integrated them into functions to make the executable code more concise.

--- video_editing.py
+++ video_editing.py
@@ -1,23 +1,23 @@
+from datetime import datetime
 import subprocess
 import argparse
 import sys
 import re
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", type=str, help="path to input file, must end with .txt", required = True)
-args = parser.parse_args()
-
-if args.file.endswith('.txt'):
-    if os.path.exists(args.file): pass
-    else:
-        print(f"Warning! '{args.file}' does not exist.")    
+## define function
+def check_file(input_file):
+    """This function checks if the file name meets the required format and if the input file exists."""
+    if not input_file.endswith('.txt'): # if not meet the required format
+        print("Warning! Input file must end with '.txt'.")
         sys.exit()
-else:
-    print("Input file must end with '.txt'.")
-    sys.exit()
+    
+    if not os.path.exists(input_file): # if not exist
+        print(f"Warning! '{input_file}' does not exist.")    
+        sys.exit()
 
-## part1
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+
 def rf(input_file):
     """This function will read the input_file text file that is passed in,and return the 
     {input stream name}, {output stream name list}, {start time list}, and {end time list}."""
@@ -27,17 +27,28 @@ def rf(input_file):
         ## record the file names to be input and output
         files = re.findall(r'\b\w+\.\w+\b', lines[0])
         in_stream, out_stream = files[0], files[1:]
+        if not os.path.exists(in_stream):
+            print(f"Warning! '{in_stream}' does not exist.")
+            sys.exit()
 
-        ## store time point 
+        ## store timestamp
         start, end = [], []
         for i in range(1,len(lines)):
             time = re.findall("[0-2][0-3]:[0-5][0-9]:[0-5][0-9]", lines[i])
-            if(len(time)==2):
+            start_obj, end_obj = datetime.strptime(time[0], '%H:%M:%S'), datetime.strptime(time[1], '%H:%M:%S')
+            if len(time)==2 and start_obj<end_obj:
                 start.append(time[0])
                 end.append(time[1])
-                print(f"Successfully read the timestamp in line {i+1}.")
+                print(f"Successfully read the timestamp on the {ordinal(i+1)} line.")
+            elif len(time)==2 and start_obj>=end_obj:
+                will = input(f"Warning! The start time of line {i+1} is greater than the end time.\nStart:[{time[0]}] End:[{time[1]}]\nDo you want to swap them and continue?(y/n)") 
+                if(will=="y"):
+                    start.append(time[1])
+                    end.append(time[0])
+                    print("Swapping completed.")
+                else:sys.exit()
             else:
-                input(f"Warning! Number of time point(s) is incorrect in line {i+1}, please correct and try again.")
+                print(f"Warning! Number of time point(s) is incorrect in line {i+1}, please correct and try again.")
                 sys.exit() 
 
         if(len(out_stream) == len(start)):
@@ -52,10 +63,14 @@ def rf(input_file):
 
         return in_stream, out_stream, start, end
 
-## categorize elements from input_file
+## execute(part1)
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str, help="path to input file, must end with .txt", required = True)
+args = parser.parse_args()
+check_file(args.file)
 in_stream, out_stream, start, end = rf(args.file)
 
-## part2
+## execute(part2)
 num_time = len(start)
 for i in range(num_time):
     try:
@@ -67,9 +82,9 @@ for i in range(num_time):
         subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         ## check whether the output success
         if os.path.exists(out_stream[i]):
-            print(f"'{out_stream[i]}' created successfully!\n")
+            print(f"'{out_stream[i]}' created successfully!")
     except:
         
-        print(f"some errors occurred while generating '{out_stream[i]}'!\n")
+        print(f"\nsome errors occurred while generating '{out_stream[i]}'!\n")
 		 
 
